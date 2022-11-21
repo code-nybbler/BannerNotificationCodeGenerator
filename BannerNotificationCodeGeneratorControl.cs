c13b834367f8b97eaa212a7e30c308ce16dd7a99
Luke Thomas <116568876+code-nybbler@users.noreply.github.com>
2022-11-21 15:25:50 -0500

    1.0.0.2
    Functionality added to allow user to automatically create web resource from within the plugin.

--- BannerNotificationCodeGeneratorControl.cs
+++ BannerNotificationCodeGeneratorControl.cs
@@ -1,8 +1,17 @@
 ﻿using McTools.Xrm.Connection;
 using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Client;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Query;
 using System;
+using System.Linq;
+using System.Reflection;
+using System.ServiceModel;
 using System.Text.RegularExpressions;
+using System.Web.Services.Description;
+using System.Web.UI.WebControls;
 using System.Windows.Forms;
+using System.Xml.Linq;
 using XrmToolBox.Extensibility;
 
 namespace BannerNotificationCodeGenerator
@@ -10,6 +19,7 @@ namespace BannerNotificationCodeGenerator
     public partial class BannerNotificationCodeGeneratorControl : PluginControlBase
     {
         private Settings mySettings;
+        private string script, webResourceName, webResourceDisplayName, webResourceDesc, solutionName;
 
         public BannerNotificationCodeGeneratorControl()
         {
@@ -64,12 +74,12 @@ namespace BannerNotificationCodeGenerator
                 string entityTagPrefix = Regex.Match(fetchXML, "<entity name=[\"']").ToString();
                 string entity = fetchXML.Substring(fetchXML.IndexOf(entityTagPrefix) + entityTagPrefix.Length, Regex.Match(fetchXML, "<entity name=[\"'](?i)[a-z_]+[\"']").ToString().Replace(entityTagPrefix, "").Length - 1);
 
-                string script = string.Format(@"function toggleBannerNotification(executionContext) {{
+                script = string.Format(@"function toggleBannerNotification(executionContext) {{
                     var formContext = executionContext.getFormContext();
                     var recordID = formContext.data.entity.getId();
 	                var fetchXML = `{0}`;
 
-                    fetchXML = fetchXML.Replace(""$$"", recordID);    
+                    fetchXML = fetchXML.replace(""$$"", recordID);    
 	                fetchXML = '?fetchXml=' + encodeURIComponent(fetchXML);     
 
 	                // call web api
@@ -87,6 +97,7 @@ namespace BannerNotificationCodeGenerator
                 }}", fetchXML, entity, message, level);
 
                 txt_script.Text = script;
+                EnableWRInputs();
             }
             catch (Exception ex)
             {
@@ -94,9 +105,150 @@ namespace BannerNotificationCodeGenerator
             }
         }
 
+        private void CreateWebResource(string script)
+        {
+            Entity wr = new Entity("webresource");
+            wr["content"] = Base64Encode(script);
+            wr["displayname"] = webResourceDisplayName;
+            wr["description"] = webResourceDesc;
+            wr["name"] = webResourceName;
+            wr["webresourcetype"] = new OptionSetValue(3); // JS
+
+            try
+            {
+                QueryByAttribute qba = new QueryByAttribute("webresource"); // create query to check existing web resources
+                qba.ColumnSet = new ColumnSet(true);
+                qba.AddAttributeValue("name", webResourceName); // add filter for name
+
+                if (Service.RetrieveMultiple(qba).Entities.FirstOrDefault() == null) // does not already exist
+                {
+                    try
+                    {
+                        CreateRequest cr = new CreateRequest
+                        {
+                            Target = wr
+                        };
+                        cr.Parameters.Add("SolutionUniqueName", solutionName);
+                        CreateResponse cresp = (CreateResponse)Service.Execute(cr);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Could not create web resource: " + ex.Message);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("A web resource with this name already exists.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Could not retrieve a list of existing web resources to check for name conflicts: " + ex.Message);
+            }
+        }
+
+        private void btn_CreateWR_Click(object sender, EventArgs e)
+        {
+            DisableWRInputs();
+
+            solutionName = box_Solution.SelectedItem.ToString();
+            webResourceName = txt_WRName.Text;
+            webResourceDisplayName = txt_WRDisplayName.Text;
+            webResourceDesc = txt_WRDesc.Text;
+
+            WorkAsync(new WorkAsyncInfo
+            {
+                Message = "Creating web resource...",
+                Work = (w, e2) =>
+                {
+                    // This code is executed in another thread
+                    CreateWebResource(script);
+
+                    w.ReportProgress(-1, "Web resource created successfully.");
+                    e2.Result = 1;
+                },
+                ProgressChanged = e2 =>
+                {
+                    SetWorkingMessage(e2.UserState.ToString());
+                },
+                PostWorkCallBack = e2 =>
+                {
+                    EnableWRInputs();
+                },
+                AsyncArgument = null,
+                // Progress information panel size
+                MessageWidth = 340,
+                MessageHeight = 150
+            });
+        }
+
+        private static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return Convert.ToBase64String(plainTextBytes);
+        }
+
+        private void txt_fetch_TextChanged(object sender, EventArgs e)
+        {
+            //DisableWRInputs();
+        }
+
+        private void EnableWRInputs()
+        {
+            box_Solution.Enabled = true;
+            txt_WRName.Enabled = true;
+            txt_WRDisplayName.Enabled = true;
+            txt_WRDesc.Enabled = true;
+            btn_CreateWR.Enabled = true;
+        }
+
+        private void DisableWRInputs()
+        {
+            box_Solution.Enabled = false;
+            txt_WRName.Enabled = false;
+            txt_WRDisplayName.Enabled = false;
+            txt_WRDesc.Enabled = false;
+            btn_CreateWR.Enabled = false;
+        }
+
+        private void RetrieveSolutions()
+        {
+            QueryExpression query = new QueryExpression
+            {
+                EntityName = "solution",
+                ColumnSet = new ColumnSet("uniquename"),
+                Criteria = new FilterExpression()
+            };
+
+            query.Criteria.AddCondition("ismanaged", ConditionOperator.Equal, false);
+
+            try
+            {
+                foreach (Entity solution in Service.RetrieveMultiple(query).Entities)
+                {
+                    if (solution["uniquename"].ToString() != "System" && solution["uniquename"].ToString() != "Active" && solution["uniquename"].ToString() != "Basic" && solution["uniquename"].ToString() != "ActivityFeeds")
+                    {
+                        box_Solution.Items.Add(solution["uniquename"]);
+                    }
+                }
+            }
+            catch (FaultException<OrganizationServiceFault> ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        private void box_Solution_Click(object sender, EventArgs e)
+        {
+            box_Solution.Items.Clear();
+            RetrieveSolutions();
+        }
+
         private void btn_Copy_Click(object sender, EventArgs e)
         {
             Clipboard.SetText(txt_script.Text);
+
+            MessageBox.Show("Copied to clipboard!");
         }
 
         private void lst_level_SelectedIndexChanged(object sender, EventArgs e)
--- BannerNotificationCodeGeneratorControl.designer.cs
+++ BannerNotificationCodeGeneratorControl.designer.cs
@@ -41,7 +41,18 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.btn_Copy = new System.Windows.Forms.Button();
+            this.btn_CreateWR = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txt_WRDesc = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txt_WRDisplayName = new System.Windows.Forms.TextBox();
+            this.txt_WRName = new System.Windows.Forms.TextBox();
+            this.box_Solution = new System.Windows.Forms.ComboBox();
             this.toolStripMenu.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // toolStripMenu
@@ -76,6 +87,7 @@
             this.txt_fetch.Name = "txt_fetch";
             this.txt_fetch.Size = new System.Drawing.Size(1060, 937);
             this.txt_fetch.TabIndex = 5;
+            this.txt_fetch.TextChanged += new System.EventHandler(this.txt_fetch_TextChanged);
             // 
             // label1
             // 
@@ -105,7 +117,7 @@
             this.txt_script.Name = "txt_script";
             this.txt_script.ReadOnly = true;
             this.txt_script.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.txt_script.Size = new System.Drawing.Size(1060, 742);
+            this.txt_script.Size = new System.Drawing.Size(1060, 499);
             this.txt_script.TabIndex = 9;
             // 
             // label2
@@ -160,7 +172,7 @@
             // 
             this.btn_Copy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_Copy.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.btn_Copy.Location = new System.Drawing.Point(2056, 229);
+            this.btn_Copy.Location = new System.Drawing.Point(2058, 229);
             this.btn_Copy.Name = "btn_Copy";
             this.btn_Copy.Size = new System.Drawing.Size(75, 36);
             this.btn_Copy.TabIndex = 15;
@@ -168,10 +180,116 @@
             this.btn_Copy.UseVisualStyleBackColor = true;
             this.btn_Copy.Click += new System.EventHandler(this.btn_Copy_Click);
             // 
+            // btn_CreateWR
+            // 
+            this.btn_CreateWR.Enabled = false;
+            this.btn_CreateWR.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_CreateWR.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.btn_CreateWR.Location = new System.Drawing.Point(979, 68);
+            this.btn_CreateWR.Name = "btn_CreateWR";
+            this.btn_CreateWR.Size = new System.Drawing.Size(75, 36);
+            this.btn_CreateWR.TabIndex = 16;
+            this.btn_CreateWR.Text = "Create";
+            this.btn_CreateWR.UseVisualStyleBackColor = true;
+            this.btn_CreateWR.Click += new System.EventHandler(this.btn_CreateWR_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox1.Controls.Add(this.txt_WRDesc);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txt_WRDisplayName);
+            this.groupBox1.Controls.Add(this.txt_WRName);
+            this.groupBox1.Controls.Add(this.box_Solution);
+            this.groupBox1.Controls.Add(this.btn_CreateWR);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.groupBox1.Location = new System.Drawing.Point(1073, 776);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1060, 237);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Web Resource Generation";
+            // 
+            // txt_WRDesc
+            // 
+            this.txt_WRDesc.Enabled = false;
+            this.txt_WRDesc.Location = new System.Drawing.Point(6, 110);
+            this.txt_WRDesc.Multiline = true;
+            this.txt_WRDesc.Name = "txt_WRDesc";
+            this.txt_WRDesc.Size = new System.Drawing.Size(1048, 121);
+            this.txt_WRDesc.TabIndex = 24;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(2, 87);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(89, 20);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "Description";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(538, 33);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(106, 20);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "Display Name";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(270, 33);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(157, 20);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Name (include prefix)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(2, 33);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 20);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Solution";
+            // 
+            // txt_WRDisplayName
+            // 
+            this.txt_WRDisplayName.Enabled = false;
+            this.txt_WRDisplayName.Location = new System.Drawing.Point(542, 56);
+            this.txt_WRDisplayName.Name = "txt_WRDisplayName";
+            this.txt_WRDisplayName.Size = new System.Drawing.Size(262, 26);
+            this.txt_WRDisplayName.TabIndex = 19;
+            // 
+            // txt_WRName
+            // 
+            this.txt_WRName.Enabled = false;
+            this.txt_WRName.Location = new System.Drawing.Point(274, 56);
+            this.txt_WRName.Name = "txt_WRName";
+            this.txt_WRName.Size = new System.Drawing.Size(262, 26);
+            this.txt_WRName.TabIndex = 18;
+            // 
+            // box_Solution
+            // 
+            this.box_Solution.Enabled = false;
+            this.box_Solution.FormattingEnabled = true;
+            this.box_Solution.Location = new System.Drawing.Point(6, 56);
+            this.box_Solution.Name = "box_Solution";
+            this.box_Solution.Size = new System.Drawing.Size(262, 28);
+            this.box_Solution.TabIndex = 17;
+            this.box_Solution.Click += new System.EventHandler(this.box_Solution_Click);
+            // 
             // BannerNotificationCodeGeneratorControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.btn_Copy);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
@@ -189,6 +307,8 @@
             this.Load += new System.EventHandler(this.BannerNotificationCodeGeneratorControl_Load);
             this.toolStripMenu.ResumeLayout(false);
             this.toolStripMenu.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -208,5 +328,15 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button btn_Copy;
+        private System.Windows.Forms.Button btn_CreateWR;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txt_WRDesc;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txt_WRDisplayName;
+        private System.Windows.Forms.TextBox txt_WRName;
+        private System.Windows.Forms.ComboBox box_Solution;
     }
 }
